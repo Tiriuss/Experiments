2bed989040fc3b1c32c37bb783b7a85f43bbb6cf
v.o.troyan888@gmail.com <v.o.troyan888@gmail.com>
2019-06-12 18:19:52 +0300

    changed

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,9 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             string name = "Tirius";
+            Console.WriteLine(name);
+
+            Console.Read();
           }
     }
 }
